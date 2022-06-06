f5894df3f3a19cb9aa2cba114ffe05a7b87d4aeb
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-05 21:56:28 -0400

    fare matrix and link to book complete

--- assets/js/feature.js
+++ assets/js/feature.js
@@ -1,41 +1,25 @@
-function convertCurrency() {
-    console.log("Currency Converter Started");
-
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "OIcMcHi6isYEUys1SbJ7MAtEOgI1151B");
-
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        headers: myHeaders
-    };
-
-    fetch(`https://api.apilayer.com/currency_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-}
-
-let amadeusTokenTEST = "1myn9L2tjIIBl2AcTBDRk7T3nKTZ"
-
-function CarrierAirline() {
-
-    let carrierCodeURL = `https://test.api.amadeus.com/v1/reference-data/airlines?airlineCodes=${carrierCode}`;
-    fetch(carrierCodeURL, {
+function carrierAirline(carrierCode, linkToBookEl) {
+    //let carrierCode = "F9"
+    const options = {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer ' + amadeusTokenTEST,
+            'X-RapidAPI-Host': 'iata-and-icao-codes.p.rapidapi.com',
+            'X-RapidAPI-Key': 'bf98be93bcmsh29c5cad60fcbe7cp1c13e7jsnffaac73a1ecd'
         }
-    })
+    };
+
+    fetch(`https://iata-and-icao-codes.p.rapidapi.com/airline?iata_code=${carrierCode}`, options)
         .then(resp => {
             if (!resp.ok) throw new Error(resp.statusText);
             return resp.json();
         })
         .then(data => {
             console.log("Data Retrieved for Fare Matrix");
-            createFareMatrix(data);
+            airlineBookingURL = data[0].name.split(" ").join("");
+            linkToBookEl.innerHTML = `<a href="https://${airlineBookingURL}.com" target="_blank"><button class="btn btn-outline-success my-2 my-sm-0 text-light">Book Ticket</button></a>`;
+            createLinkToBook(data);
 
         })
         .catch(console.err);
-}
+    };
+carrierAirline()
